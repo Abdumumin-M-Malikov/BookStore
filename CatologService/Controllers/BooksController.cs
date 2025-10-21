869d50ad52459c08abb37fe71cea96a591176c80
unknown <Abdumumin-M-Malikov>
2025-10-21 14:09:43 +0500

    went to CRUD

---
+++ CatologService/Controllers/BooksController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CatologService.Models;
+using Microsoft.EntityFrameworkCore;
+using CatologService.Data;
+
+
+namespace CatologService.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BooksController : ControllerBase
+    {
+      private readonly CatalogDbContext _context;
+        public BooksController(CatalogDbContext context)
+        { 
+        _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var books = await _context.Books.ToListAsync();
+            return Ok(books);
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if(book==null) return NotFound();
+            return Ok(book);
+        }
+        public async Task<IActionResult> Create(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = book.Id}, book);
+        }
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, Book book)
+        {
+            if (id != book.Id) return BadRequest();
+            var exists = await _context.Books.AnyAsync(x=>x.Id==id);
+            if(!exists) return NotFound();
+            _context.Entry(book).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+    }
+}
---
+++ CatologService/Data/CatalogDbContext.cs
@@ -0,0 +1,10 @@
+﻿using CatologService.Models;
+using Microsoft.EntityFrameworkCore;
+namespace CatologService.Data
+{
+    public class CatalogDbContext:DbContext
+    {
+        public CatalogDbContext(DbContextOptions<CatalogDbContext> options) : base(options) { }
+        public DbSet<Book> Books { get; set; }
+    }
+}
